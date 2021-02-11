5204e5cf22d4423ff0991480d553f0b71a047250
Ryan D <p21rdentchev@fairfieldprep.org>
2021-02-11 13:15:34 -0500

    lab 3s

---
+++ lab_3-1.py
@@ -0,0 +1,4 @@
+# Ryan Dentchev 2/11/2021
+
+with open("my_file.txt", "w") as newfile:
+    newfile.write("R D 2/11/2021\nHello World! \nFishing\n")
---
+++ lab_3-2.py
@@ -0,0 +1,5 @@
+# Ryan Dentchev 2/11/2021
+
+with open("my_file.txt", "a") as new_file:
+    new_file.write("Facetime\n")
+    new_file.write("Go to the beach\n")
